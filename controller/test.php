81e4d94d968d6b5c83201bc68358309c1a622679
ETY001 <ety001@domyself.me>
2015-04-07 10:30:47 +0800

    购买服务测试用例

--- controller/test.php
+++ controller/test.php
@@ -1,8 +1,27 @@
 <?php
 class test extends spController
 {
-    public function tt(){
-        spClass('m_port_pool')->updateField(array('port'=>10000), 'status', 1);
+    public function testBuyService(){
+        $arr = array(
+            'service_id'    => 1,
+            'user_id'       => 1,
+            'buy_time'      => time()
+        );
+        $buyservice_lib     = spClass('m_buyservice');
+        $user_lib           = spClass('m_user');
+        $chk                = $user_lib->chk_money(1, 1);
+        if(!$chk){
+            echo '余额不足';
+            return;
+        }
+
+        $chk_service        = $buyservice_lib->chk_service(1);
+        if($chk_service){
+            echo '有正在使用的服务';
+            return;
+        }
+
+        echo $buyservice_lib->save_service($arr);
     }
 
     public function email(){
