7779bf1a726fac28570f5e0d9def52ba61bde4ce
ETY001 <ety001@domyself.me>
2015-09-09 03:55:09 +0800

    function conflict

--- inc/alipay/func.php
+++ inc/alipay/func.php
@@ -68,18 +68,6 @@ function argSort($para) {
     reset($para);
     return $para;
 }
-/**
- * 写日志，方便测试（看网站需求，也可以改成把记录存入数据库）
- * 注意：服务器需要开通fopen配置
- * @param $word 要写入日志里的文本内容 默认值：空值
- */
-function logResult($word='') {
-    $fp = fopen("log.txt","a");
-    flock($fp, LOCK_EX) ;
-    fwrite($fp,"执行日期：".strftime("%Y%m%d%H%M%S",time())."\n".$word."\n");
-    flock($fp, LOCK_UN);
-    fclose($fp);
-}
 
 /**
  * 远程获取数据，POST模式
